b4a9303cf287b83067021e11744ae438e8c03420
youheiss <youhei666@outlook.com>
2023-07-21 15:50:53 +0800

    0

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
     let Play_begins = false;
     var Fullpage = new fullpage('#fullpage', {
-        anchors: ['page1', 'page2', 'page3', 'page4'],//页码后缀
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],//页码后缀
         loopTop: true,//无限循环
         loopBottom: true,
-        // navigation: true,//开启导航条
-        navigationTooltips: ['Page 1', 'Page 2', 'Page 3', 'Page 4'],//导航提示内容
+        autoScrolling: false,
+        navigation: true,//开启导航条
+        navigationTooltips: ['首页', '介绍', '功能', '展示', '关注'],//导航提示内容
         showActiveTooltip: true, //显示导航提示
         // credits: false,
         afterLoad: function (origin, destination, direction) {
-            if (destination.index == 1) {
-                $("#section1 img").css("left", 0 + "px")
-            } else {
-                $("#section1 img").css("left", "100%")
-            }
             if (destination.index == 2) {
                 if (Play_begins == true) { return; }
                 window.postMessage({ type: 'Detect_installation_status', data: "莫西莫西,盘络你在吗?" }, '*');
@@ -24,6 +20,8 @@ $(document).ready(function () {
         }
 
     });
+    
+
     $("#section2 .mx-auto li").hover(function () {
         $(this).addClass("active").siblings().removeClass("active");
     })
